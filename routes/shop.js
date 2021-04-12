d72dcdb452cabb846d878662f5a290b234e1cbb1
nilay personal <nilaysachan16@gmail.com>
2021-04-12 23:05:50 +0530

    Adding orders in navigation menu_corresponding routes ,controllers and views

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,7 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
