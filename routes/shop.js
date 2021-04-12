117fd3b1fb89a86c16972efa426ba20ce10df612
nilay personal <nilaysachan16@gmail.com>
2021-04-12 22:49:00 +0530

    Refactoring the code_Adding different navigation menuoptions_Adding corresponding controllers & views

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 const express = require('express');
 
 const rootDir = require('../util/path');
@@ -8,6 +8,12 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
