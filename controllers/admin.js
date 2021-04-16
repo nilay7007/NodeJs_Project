00a4cc50c8ff621344cc67058a226d701cad6dd6
nilay personal <nilaysachan16@gmail.com>
2021-04-16 22:04:11 +0530

    Adding edit item functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,9 @@
 const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
+    editing: false
     });
   };
   
@@ -27,6 +25,43 @@ exports.getAddProduct = (req, res, next) => {
         path: '/admin/products'
       });
     });
+  };
+ 
+  
+  exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+      return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    });
+  };
+
+  exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(
+      prodId,
+      updatedTitle,
+      updatedImageUrl,
+      updatedDesc,
+      updatedPrice
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
   };
   
   
